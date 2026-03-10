7bac6f47dfea07540b6fba4889b9a195572be33a
mrsaifu08 <saifstack25@gmail.com>
2026-03-10 11:51:35 +0530

    client setup

---
+++ client/src/components/Sidebar.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import assets, { userDummyData } from "./../assets/assets";
+import { useNavigate } from "react-router-dom";
+
+const Sidebar = ({ selectedUser, setSelectedUser }) => {
+  const navigate = useNavigate();
+  return (
+    <div
+      className={`bg-[#8185b2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${selectedUser ? "max-md:hidden" : ""}`}
+    >
+      <div className="p-5">
+        <div className="flex justify-between items-center">
+          <img src={assets.logo} alt="logo" className="max-w-40" />
+          <div className="relative p-2 group">
+            <img
+              src={assets.menu_icon}
+              alt="menu_icon"
+              className="max-h-5 cursor-pointer"
+            />
+            <div className="absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#282142] border border-gray-600 text-gray-100 hidden group-hover:block">
+              <p
+                onClick={() => navigate("/profile")}
+                className="cursor-pointer text-sm"
+              >
+                Edit Profile
+              </p>
+              <hr className="my-2 border-t border-gray-500" />
+              <p className="cursor-pointer text-sm">Logout</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-[#282142] rounded-full flex items-center gap-2 py-3 px-4 mt-5">
+          <img src={assets.search_icon} alt="search" className="w-3"/>
+          <input type="text" className="bg-transparent border-none outline-none text-white text-xs flex-1 placeholder-[#c8c8c8]" placeholder="Search user..."/>
+        </div>
+      </div>
+
+      <div className="flex flex-col">
+        {userDummyData.map((user, index) => (
+          <div>
+            <img src={user?.profilePic || assets.avatar_icon} alt="" className="w-[35px] aspect-[1/1] rounded-full" />
+            <div className="flex flex-col leading-5">
+
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,56 @@
+// // import React from "react";
+// // import ChatContainer from "../components/ChatContainer";
+// // import Sidebar from "../components/Sidebar";
+// // import RightSidebar from "../components/RightSidebar";
+
+// // const HomePage = () => {
+// //   return (
+// //     <div className="w-full h-screen border sm:px-[15%] sm:py-[5%]">
+// //       <div className="backdrop-blur-xl border-2 border-y-gray-600 rounded-2xl overflow-hidden h-full grid grid-cols-1 relative">
+// //         <Sidebar />
+// //         <ChatContainer />
+// //         <RightSidebar />
+// //       </div>
+// //     </div>
+// //   );
+// // };
+
+// // export default HomePage;
+
+import React, { useState } from "react";
+import ChatContainer from "../components/ChatContainer";
+import Sidebar from "../components/Sidebar";
+import RightSidebar from "../components/RightSidebar";
+import bgImage from "../assets/bgImage.svg";
+
+const HomePage = () => {
+
+  const [selectedUser, setSelectedUser] = useState(false)
+
+  return (
+    <div
+      className="w-full h-screen flex items-center justify-center p-6 bg-gray-950"
+      style={{
+        backgroundImage: `url(${bgImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div
+        className={`w-full max-w-[1400px] h-full
+        grid grid-cols-[260px_1fr_300px]
+        bg-white/5 backdrop-blur-xl
+        border border-white/10
+        rounded-2xl
+        shadow-[0_20px_60px_rgba(0,0,0,0.7)]
+        overflow-hidden ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}
+      >
+        <Sidebar />
+        <ChatContainer />
+        <RightSidebar />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
