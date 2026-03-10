9e876ca2f38b6a8830bfd220b859fc152271c55c
mrsaifu08 <saifstack25@gmail.com>
2026-03-10 14:35:42 +0530

    Sidebar UI Done

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -38,11 +38,15 @@ const Sidebar = ({ selectedUser, setSelectedUser }) => {
 
       <div className="flex flex-col">
         {userDummyData.map((user, index) => (
-          <div>
+          <div onClick={()=> {setSelectedUser(user)}} key={index} className={`relative flex items-center gap-2 pl-4 p-2 rounded cursor-pointer max-sm:text-sm ${selectedUser?._id === user._id && 'bg-[#282142]/50'}`}>
             <img src={user?.profilePic || assets.avatar_icon} alt="" className="w-[35px] aspect-[1/1] rounded-full" />
             <div className="flex flex-col leading-5">
-
+              <p>{user.fullName}</p>
+              {
+                index < 3 ?  <span className="text-green-400 text-sm">Online</span> : <span className="text-neutral-400 text-sm">Offline</span>
+              }
             </div>
+            {index > 2 && <p className="absolute top-4 right-4 text-xs h-5 w-5 flex justify-center items-center rounded-full bg-violet-500/50">{index}</p>}
           </div>
         ))}
       </div>
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -24,11 +24,20 @@ const HomePage = () => {
         border border-white/10
         rounded-2xl
         shadow-[0_20px_60px_rgba(0,0,0,0.7)]
-        overflow-hidden ${selectedUser ? 'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}
+        overflow-hidden ${selectedUser ? "md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]" : "md:grid-cols-2"}`}
       >
-        <Sidebar />
-        <ChatContainer />
-        <RightSidebar />
+        <Sidebar
+          selectedUser={selectedUser}
+          setSelectedUser={setSelectedUser}
+        />
+        <ChatContainer
+          selectedUser={selectedUser}
+          setSelectedUser={setSelectedUser}
+        />
+        <RightSidebar
+          selectedUser={selectedUser}
+          setSelectedUser={setSelectedUser}
+        />
       </div>
     </div>
   );
