67f0c5fbc253013f8cc36794388f1c9826b44c16
mrsaifu08 <saifstack25@gmail.com>
2026-03-10 14:47:45 +0530

    ChatContainer UI Done

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import assets from '../assets/assets'
 
-const RightSidebar = () => {
-  return (
-    <div>RightSidebar</div>
+const RightSidebar = ({selectedUser}) => {
+  return selectedUser && (
+    <div>
+      <div>
+        <img src={selectedUser?.profilePic || assets.avatar_icon} alt="" className='w-15 aspect-[1/1] rounded-full' />
+        <h1 className='px-10 text-xl font-medium mx-auto flex items-center gap-2'>
+          <p className='w-2 h-2 rounded-full bg-green-500'></p>
+          {selectedUser.fullName}
+        </h1>
+        <p className='px-10 mx-auto'>{selectedUser.bio}</p>
+      </div>
+    </div>
   )
 }
 
