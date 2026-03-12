dd865628625dda5a87a6fea68ad7e419bda1ba55
mrsaifu08 <saifstack25@gmail.com>
2026-03-12 14:42:27 +0530

    feat: add MongoDB database connection

--- server/lib/db.js
+++ server/lib/db.js
@@ -1,11 +1,11 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// export const connectDB = async () => {
-//   try {
-//     await mongoose.connect(`${process.env.MONGODB_URI}/chat-app`);
-//     console.log("Database connected");
-//   } catch (error) {
-//     console.error("Database connection failed:", error.message);
-//     throw error; // server.js mein catch hoga
-//   }
-// };
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/chat-app`);
+    console.log("Database connected");
+  } catch (error) {
+    console.error("Database connection failed:", error.message);
+    throw error; // server.js mein catch hoga
+  }
+};
