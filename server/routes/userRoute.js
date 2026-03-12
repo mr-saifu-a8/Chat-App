0c2d59ee81c16e1db5f0e38e3ff123f2aaad4169
mrsaifu08 <saifstack25@gmail.com>
2026-03-12 14:48:45 +0530

    userRoute:

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,12 +1,12 @@
-// import express from "express"
-// import { checkAuth, login, signup, updateProfile } from "../controllers/userController.js"
-// import { protectedRoutes } from "../middlewares/auth.js"
+import express from "express"
+import { checkAuth, login, signup, updateProfile } from "../controllers/userController.js"
+import { protectedRoutes } from "../middlewares/auth.js"
 
-// const userRouter = express.Router()
+const userRouter = express.Router()
 
-// userRouter.post("/signup", signup)
-// userRouter.post("/login", login)
-// userRouter.post("/updated-profile", protectedRoutes, updateProfile)
-// userRouter.get("/check", protectedRoutes, checkAuth)
+userRouter.post("/signup", signup)
+userRouter.post("/login", login)
+userRouter.post("/updated-profile", protectedRoutes, updateProfile)
+userRouter.get("/check", protectedRoutes, checkAuth)
 
-// export default userRouter
+export default userRouter
