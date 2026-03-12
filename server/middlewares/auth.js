cc01d4196771679f958ea47fe50ad32329340c45
mrsaifu08 <saifstack25@gmail.com>
2026-03-12 14:43:50 +0530

    feat: auth middleware

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,31 +1,31 @@
-// import User from "../models/User.js";
-// import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
 
-// export const protectedRoutes = async (req, res, next) => {
-//   try {
-//     const token = req.headers.authorization?.split(" ")[1];
+export const protectedRoutes = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
 
-//     if (!token) {
-//       return res
-//         .status(401)
-//         .json({ success: false, message: "No token, access denied" });
-//     }
+    if (!token) {
+      return res
+        .status(401)
+        .json({ success: false, message: "No token, access denied" });
+    }
 
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     const user = await User.findById(decoded.userId).select("-password");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findById(decoded.userId).select("-password");
 
-//     if (!user) {
-//       return res
-//         .status(404)
-//         .json({ success: false, message: "User not found" });
-//     }
+    if (!user) {
+      return res
+        .status(404)
+        .json({ success: false, message: "User not found" });
+    }
 
-//     req.user = user;
-//     next();
-//   } catch (error) {
-//     console.log(error.message);
-//     res
-//       .status(401)
-//       .json({ success: false, message: "Invalid or expired token" });
-//   }
-// };
+    req.user = user;
+    next();
+  } catch (error) {
+    console.log(error.message);
+    res
+      .status(401)
+      .json({ success: false, message: "Invalid or expired token" });
+  }
+};
